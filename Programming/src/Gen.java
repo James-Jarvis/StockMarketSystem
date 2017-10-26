f35af45bd57fcd77d472eff21bd715ab14913988
Selwes97 <15564357@students.lincoln.ac.uk>
2017-10-26 15:23:51 +0100

    added the thread examples from the WA4 workshop

--- Programming/src/Gen.java
+++ Programming/src/Gen.java
@@ -2,6 +2,16 @@ public class Gen {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
 
+        HelloWorld hw1 = new HelloWorld(1);
+        HelloWorld hw2 = new HelloWorld(2);
+
+        Thread t1 = new Thread(hw1);
+        Thread t2 = new Thread(hw2);
+
+        t1.start();
+        t2.start();
+
+        System.out.println("end");
+    }
 }
--- Programming/src/HelloWorld.java
+++ Programming/src/HelloWorld.java
@@ -1,5 +1,19 @@
-public class HelloWorld {
+import java.util.*;
 
+public class HelloWorld extends Thread {
 
+    int name;
+
+    Random rnd = new Random();
+
+    public HelloWorld (int nm) {
+        name = nm;
+    }
+
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("I am Program " + name + ": " + rnd.nextInt(40));
+        }
+    }
 
 }
