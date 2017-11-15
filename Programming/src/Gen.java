87ea55e1bf066fc05400829303a745712e972479
Sam Elwes <samelwes@btinternet.com>
2017-11-15 10:55:40 +0000

    made register class

--- Programming/src/Gen.java
+++ Programming/src/Gen.java
@@ -3,10 +3,8 @@ public class Gen {
     public static void main(String[] args) {
         System.out.println("Connecting to socket... fingers crossed!");
 
-        Connect con = new Connect();
-
+        Connect con = new Connect("localhost");
         Thread sendThread = new Thread(con);
-
         sendThread.start();
 
 
--- Programming/src/Register.java
+++ Programming/src/Register.java
@@ -1,14 +1,14 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class Connect extends Thread {
+public class Register extends Thread {
 
     String connectionIP;
 
-
-    public Connect (String ip) {
+    public Register (String ip) {
         connectionIP = ip;
     }
 
@@ -52,3 +52,4 @@ public class Connect extends Thread {
     }
 
 }
+
